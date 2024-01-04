0711f9c62ccbfc68db167ea1975d1adfbe9ca98f
Joon <joohyun.kwon@gmail.com>
2024-01-04 00:56:53 -0500

    cleaned up database_controller_test

--- internal/controller/database_controller_test.go
+++ internal/controller/database_controller_test.go
@@ -14,54 +14,51 @@ import (
 	postgresv1 "postgres-aurora-db-user/api/v1"
 )
 
-var _ = Describe("Database controller", func() {
+var _ = Describe("Database controller create and delete", Ordered, func() {
 
-	// Define utility constants for object names and testing timeouts/durations and intervals.
-	const (
-		DatabaseObjectName = "test-db"
-		DatabaseNamespace  = "default"
-	)
+	Context("When create database custom resource on K8s", func() {
+		var (
+			DatabaseName      = "test-db"
+			DatabaseNamespace = "default"
+			ctx               = context.Background()
+		)
+		It("Should call create operation successfully", func() {
 
-	Context("When updating Database Status", func() {
-		It("Should Database has to be created", func() {
-			By("By creating a new database")
-			ctx := context.Background()
-			Database := &postgresv1.Database{
+			database := &postgresv1.Database{
 				TypeMeta: metav1.TypeMeta{
 					APIVersion: "postgres.aurora.operator.k8s/api/v1",
 					Kind:       "Database",
 				},
 				ObjectMeta: metav1.ObjectMeta{
-					Name:      DatabaseObjectName,
+					Name:      DatabaseName,
 					Namespace: DatabaseNamespace,
 				},
 				Spec: postgresv1.DatabaseSpec{},
 			}
-			Expect(k8sClient.Create(ctx, Database)).Should(Succeed())
+			Expect(k8sClient.Create(ctx, database)).Should(Succeed())
+		})
 
+		It("Should have the object created with necessary status configured", func() {
+			database := &postgresv1.Database{}
 			Expect(func() error {
 				var err error
-				// wait up to 60 seconds
-				for i := 1; i < 30; i++ {
+				// wait for a while for the object is created
+				for i := 1; i < 10; i++ {
 					err := k8sClient.Get(ctx, types.NamespacedName{
-						Namespace: Database.Namespace,
-						Name:      Database.Name,
-					}, Database)
-					if err == nil && Database.Status.DatabaseName != "" {
+						Namespace: DatabaseNamespace,
+						Name:      DatabaseName,
+					}, database)
+					if err == nil && database.Status.DatabaseName != "" {
 						return nil
 					}
 					time.Sleep(time.Duration(2*i) * time.Second)
 				}
 				return fmt.Errorf("timed out with err: %s", err)
 			}()).Should(Succeed())
-			database := &postgresv1.Database{}
-			k8sClient.Get(ctx, types.NamespacedName{
-				Namespace: DatabaseNamespace,
-				Name:      DatabaseObjectName,
-			}, database)
 			Expect(database.Status).To(HaveField("DatabaseName", "default_test_db"))
 			Expect(database.Status).To(HaveField("AppRoleRW", "default_test_db_role_rw"))
 			Expect(database.Status).To(HaveField("AppRoleRO", "default_test_db_role_ro"))
 		})
+
 	})
 })
